a53926a39324dec09e64e66ef99b44f4aeb4e3d0
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-08-14 22:43:44 +0430

    Practice UseState Hook

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Loading from "./Loading";
-import Tours from "./Tours";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = "https://course-api.com/react-tours-project";
+import React from "react";
+import Review from "./Review";
 function App() {
-  const [tours, setTours] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [refresh, setRefresh] = useState(false);
-
-  const refreshTours = () => {
-    setRefresh(!refresh);
-  };
-
-  const getData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setTours(tours);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  const deleteTour = (id) => {
-    const newTours = tours.filter((tour) => {
-      if (id !== tour.id) {
-        return tour;
-      }
-    });
-    setTours(newTours);
-  };
-
-  useEffect(() => {
-    getData();
-  }, [refresh]);
   return (
     <main>
-      {loading ? (
-        <Loading />
-      ) : (
-        <Tours
-          refreshTours={refreshTours}
-          deleteTour={deleteTour}
-          items={tours}
-        />
-      )}
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
     </main>
   );
 }
