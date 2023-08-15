6a1ebab0bf3f59b590f5e24d48571c23522734a3
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-08-15 18:00:01 +0430

    Practice UseState Hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
 import data from "./data";
-import SingleQuestion from "./Question";
+
 function App() {
+  const [items, setItems] = useState(data);
+  const [categories, setCategories] = useState([
+    ...new Set(
+      data.map((item) => {
+        return item.category;
+      })
+    ),
+  ]);
+
+  const filterItems = (category) => {
+    if (!category) {
+      setItems(data);
+    } else {
+      const newData = data.filter((item) => {
+        if (item.category === category) {
+          return item;
+        }
+      });
+      setItems(newData);
+    }
+  };
+
   return (
     <main>
-      <section className="container">
-        <h3>Questions And Answers About Login</h3>
-        <section>
-          {data.map((question) => {
-            return <SingleQuestion key={question.id} question={question} />;
-          })}
-        </section>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu menuItems={items} />
       </section>
     </main>
   );
