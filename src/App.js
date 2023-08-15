11d1284546107cbf475e7ffba50e88f5d78139de
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-08-15 18:14:58 +0430

    Practice UseState Hook

--- src/App.js
+++ src/App.js
@@ -1,59 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
+import React, { useState } from "react";
 import data from "./data";
 function App() {
-  const persons = useState(data)[0];
-  const [index, setIndex] = useState(0);
+  const [text, setText] = useState([]);
+  const [number, setNumber] = useState(0);
 
-  useEffect(() => {
-    if (index < 0) {
-      return setIndex(persons.length - 1);
-    } else if (index > persons.length - 1) {
-      return setIndex(0);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let num = parseInt(number);
+    //   // My Approach
+    //   let paragraphs = [];
+    //   if (num <= 1 || !num) {
+    //     setText([data[0]]);
+    //   } else {
+    //     for (let index = 0; index < num; index++) {
+    //       paragraphs.push(data[index]);
+    //     }
+    //     setText(paragraphs);
+    //   }
+
+    // Teacher's Approach
+    if (number <= 0) {
+      num = 1;
+    } else if (number >= data.length) {
+      num = data.length;
     }
-  }, [index, persons]);
-
-  useEffect(() => {
-    const autoSlide = setInterval(() => {
-      setIndex(index + 1);
-    }, 2000);
-    return () => clearInterval(autoSlide);
-  }, [index]);
+    console.log(data.length);
+    setText(data.slice(0, num));
+  };
 
   return (
-    <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>Reviews
-        </h2>
-      </div>
-      <div className="section-center">
-        {persons.map((person, personIndex) => {
-          const { id, name, image, title, quote } = person;
-          let position = "nextSlide";
-          if (personIndex === index) {
-            position = "activeSlide";
-          }
-          if (
-            personIndex === index - 1 ||
-            (index === 0 && personIndex === persons.length - 1)
-          ) {
-            position = "lastSlide";
-          }
-          return (
-            <article key={id} className={position}>
-              <img src={image} alt={name} className="person-img" />
-              <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              {<FaQuoteRight className="icon" />}
-            </article>
-          );
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="paragraphs">Paragraphs:</label>
+        <input
+          type="number"
+          onChange={(event) => setNumber(event.target.value)}
+          value={number}
+          name="paragraphs"
+          id="paragraphs"
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((text, index) => {
+          return <p key={index}>{text}</p>;
         })}
-        <FiChevronLeft className="prev" onClick={() => setIndex(index - 1)} />
-        <FiChevronRight className="next" onClick={() => setIndex(index + 1)} />
-      </div>
+      </article>
     </section>
   );
 }
