f3cdffc4c9d1dd5d0fb78a26dce11153e677482e
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-08-22 19:47:34 +0430

    LocalStorage is added

--- src/Alert.js
+++ src/Alert.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 
-const Alert = ({ alert }) => {
+const Alert = ({ alert, removeAlert, list }) => {
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  }, [list]);
   return <section className={`alert alert-${alert.type}`}>{alert.msg}</section>;
 };
 
--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   const [grocery, setGrocery] = useState("");
   const [isEditing, setIsEditing] = useState(false);
   const [editGrocery, setEditGrocery] = useState(null);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
 
   useEffect(() => {
-    const timeOut = setTimeout(() => {
-      setAlert({ show: false, msg: "", type: "" });
-    }, 3000);
-    return () => clearTimeout(timeOut);
-  }, [alert]);
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, msg, type });
+  };
 
   const deleteItemHandler = (id) => {
     const newList = list.filter((item) => {
@@ -23,7 +33,7 @@ function App() {
       }
     });
     setList(newList);
-    setAlert({ show: true, msg: "Item Removed", type: "danger" });
+    showAlert(true, "danger", "Item Removed");
   };
 
   const editItemHandler = (id) => {
@@ -41,21 +51,22 @@ function App() {
     event.preventDefault();
     if (isEditing) {
       if (!grocery) {
-        setAlert({ show: true, msg: "Please Enter Value", type: "danger" });
+        showAlert(true, "danger", "Please Enter Value");
       } else {
         const items = list.map((item) => {
           if (item.id === editGrocery) {
-            item.name = grocery;
+            return { ...item, name: grocery };
           }
           return item;
         });
         setList(items);
         setGrocery("");
         setIsEditing(false);
-        setAlert({ show: true, msg: "Value Changed", type: "success" });
+        showAlert(true, "success", "Value Changed");
+        setEditGrocery(null);
       }
     } else if (!grocery) {
-      setAlert({ show: true, msg: "Please Enter Value", type: "danger" });
+      showAlert(true, "danger", "Please Enter Value");
     } else {
       const newGrocery = {
         id: new Date().getTime().toString(),
@@ -63,14 +74,16 @@ function App() {
       };
       setList((list) => [...list, newGrocery]);
       setGrocery("");
-      setAlert({ show: true, msg: "Item Added To The List", type: "success" });
+      showAlert(true, "success", "Item Added To The List");
     }
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert["show"] && <Alert alert={alert} />}
+        {alert["show"] && (
+          <Alert alert={alert} list={list} removeAlert={showAlert} />
+        )}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -95,11 +108,7 @@ function App() {
           <button
             onClick={() => {
               setList([]);
-              setAlert({
-                show: true,
-                msg: "Empty List",
-                type: "danger",
-              });
+              showAlert(true, "danger", "Empty List");
             }}
             type="button"
             className="clear-btn"
