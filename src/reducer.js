d428806f5ee8e8efac1f3b3374b3ec1dd52c15ea
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-09-15 17:41:49 +0430

    Increase item functionality is completed

--- src/reducer.js
+++ src/reducer.js
@@ -1,21 +1,19 @@
 const reducer = (state, action) => {
   if (action.type === "CLEAR CART") {
-    console.log("clear cart");
-    return {
-      ...state,
-      cart: [],
-    };
+    return { ...state, cart: [] };
   }
   if (action.type === "REMOVE ITEM") {
-    console.log("remove item");
     const newCart = state.cart.filter((item) => item.id !== action.payload);
-    return {
-      ...state,
-      cart: newCart,
-    };
+    return { ...state, cart: newCart };
   }
   if (action.type === "INCREASE ITEM") {
-    console.log("increase item");
+    let newCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: newCart };
   }
   if (action.type === "DECREASE ITEM") {
     console.log("decrease item");
