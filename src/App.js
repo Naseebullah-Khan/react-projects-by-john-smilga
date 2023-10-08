a2036282fe94b6ea6ae2110900c6e42a08101c4c
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-08 21:51:40 +0430

    Introduced react-router-dom Link

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 
+// Link in react-router-dom: When you want to navigate through your project
+// a-> href -> When you want to go to outside of your project.
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"about"} element={<About />} />
-        <Route path={"products"} element={<Products />} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
