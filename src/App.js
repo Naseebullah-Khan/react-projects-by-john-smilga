515964cc0dbb7eec5567bb04e9ebf92f5e4f02e3
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-08 21:22:27 +0430

    Initial setup of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import Setup from "./tutorial/1-useState/setup/1-error-example";
-// import Final from "./tutorial/1-useState/final/1-error-example";
-// import Setup from "./tutorial/1-useState/setup/2-useState-basics";
-// import Final from "./tutorial/1-useState/final/2-useState-basics";
-// import Setup from "./tutorial/1-useState/setup/3-useState-array";
-// import Final from "./tutorial/1-useState/final/3-useState-array";
-// import Setup from "./tutorial/1-useState/setup/4-useState-object";
-// import Final from "./tutorial/1-useState/final/4-useState-object";
-// import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-// import Final from "./tutorial/1-useState/final/5-useState-counter";
-// import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
-// import Final from "./tutorial/2-useEffect/final/1-useEffect-basics";
-// import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
-// import Final from "./tutorial/2-useEffect/final/2-useEffect-cleanup";
-// import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
-// import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
-// import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
-// import Final from "./tutorial/3-conditional-rendering/final/1-multiple-returns";
-// import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
-// import Final from "./tutorial/3-conditional-rendering/final/2-short-circuit";
-// import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
-// import Final from "./tutorial/3-conditional-rendering/final/3-show-hide";
-// import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
-// import Final from "./tutorial/4-forms/final/1-controlled-inputs";
-// import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
-// import Final from "./tutorial/4-forms/final/2-multiple-inputs";
-// import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
-// import Final from "./tutorial/5-useRef/final/1-useRef-basics";
-// import Setup from "./tutorial/6-useReducer/setup/";
-// import Final from "./tutorial/6-useReducer/final";
-// import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
-// import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
-// import Setup from "./tutorial/8-useContext/setup/1-context-api";
-// import Final from "./tutorial/8-useContext/final/1-context-api";
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
-// import Final from "./tutorial/9-custom-hooks/final/1-fetch-example";
-// import Setup from "./tutorial/10-prop-types/setup";
-// import Final from "./tutorial/10-prop-types/final";
-// import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
-// import Final from "./tutorial/12-memo-useMemo-useCallback/final";
 function App() {
   return (
-    <div className="container">
-      <Setup />
-      {/* <Final /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<div>Homepage</div>} />
+        <Route
+          path={"testing"}
+          element={
+            <div>
+              <h2>testing</h2>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
