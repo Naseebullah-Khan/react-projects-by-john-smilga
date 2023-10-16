c79e473bc7d36595e52df042ca25280f28591560
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-16 16:36:47 +0430

    Add navbar to project and error page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 // Link in react-router-dom: When you want to navigate through your project
 // a-> href -> When you want to go to outside of your project.
@@ -9,11 +10,14 @@ import Products from "./pages/Products";
 function App() {
   return (
     <BrowserRouter>
+      <nav>our navbar</nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
+      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
