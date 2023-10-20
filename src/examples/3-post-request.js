4e20c34d42ba3b22db314dc4d3076947960d6faf
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-20 12:32:24 +0430

    axios-tutorial

--- src/examples/3-post-request.js
+++ src/examples/3-post-request.js
@@ -1,22 +1,20 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-const Login = ({ setUser }) => {
+import axios from 'axios';
+const url = 'https://course-api.com/axios-tutorial-post';
+
+const PostRequest = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const navigate = useNavigate();
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!name || !email) return;
-    setUser({ name: name, email: email });
-    navigate('/dashboard');
+    console.log(name, email);
   };
 
   return (
-    <section className='section'>
+    <section>
+      <h2 className='text-center'>post request</h2>
       <form className='form' onSubmit={handleSubmit}>
-        <h5>login</h5>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
@@ -48,4 +46,4 @@ const Login = ({ setUser }) => {
     </section>
   );
 };
-export default Login;
+export default PostRequest;
