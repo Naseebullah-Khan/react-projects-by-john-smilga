a0c4b6d79986ae3c0bd581f4918b1e35e51def3d
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-24 17:08:06 +0430

    Practice Axios POST Request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Title from "./components/Title";
 // import Setup from "./examples/1-first-request";
 // import Final from "./final/examples/1-first-request";
-import Setup from "./examples/2-headers";
+// import Setup from "./examples/2-headers";
 // import Final from "./final/examples/2-headers";
-
+import Setup from "./examples/3-post-request";
+// import Final from "./final/examples/3-post-request";
 function App() {
   return (
     <main>
