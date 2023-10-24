d0eec52d9ecd109cf2a89f821e07c7f98abcbf7c
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-24 17:01:20 +0430

    Practice Axios Request Headers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Title from "./components/Title";
-import Setup from "./examples/1-first-request";
-import Final from "./final/examples/1-first-request";
+// import Setup from "./examples/1-first-request";
+// import Final from "./final/examples/1-first-request";
+import Setup from "./examples/2-headers";
+// import Final from "./final/examples/2-headers";
+
 function App() {
   return (
     <main>
