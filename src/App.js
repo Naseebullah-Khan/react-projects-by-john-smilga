34fc6be9b0aac236f8682ca19392ff8d286d936e
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-27 15:16:43 +0430

    Starter Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import Title from "./components/Title";
-// import Setup from "./examples/1-first-request";
-// import Final from "./final/examples/1-first-request";
-// import Setup from "./examples/2-headers";
-// import Final from "./final/examples/2-headers";
-// import Setup from "./examples/3-post-request";
-// import Final from "./final/examples/3-post-request";
-// import Setup from "./examples/4-global-instance";
-// import Final from "./final/examples/4-global-instance";
-// import "./axios/global";
-// import Setup from "./examples/5-custom-instance";
-// import Final from "./final/examples/5-custom-instance";
-import Setup from "./examples/6-interceptors";
-// import Final from "./final/examples/6-interceptors";
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
 function App() {
   return (
-    <main>
-      <Title />
-      <Setup />
-      {/* <Final /> */}
-    </main>
-  );
+    <div>
+      <h2>app component</h2>
+    </div>
+  )
 }
 
-export default App;
+export default App
