5de4a8d441e06a7447b62b58668d4a86501b4474
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-10-30 21:20:18 +0430

    Fetching Cocktails Functionality is Completed

--- src/context.js
+++ src/context.js
@@ -9,6 +9,38 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
+  const fetchCocktails = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const { drinks } = await response.json();
+      if (drinks) {
+        const newCocktails = drinks.map((drink) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            drink;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCocktails();
+  }, [searchTerm]);
+
   return (
     <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
       {children}
