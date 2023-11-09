98945f4e1603f9e6244326bda14f02fd25691402
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-09 22:04:56 +0430

    Markdown-Preview Branch is Created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import pages
-import Home from "./pages/Home";
-import About from "./pages/About";
-import SingleCocktail from "./pages/SingleCocktail";
-import Error from "./pages/Error";
-// import components
-import Navbar from "./components/Navbar";
-import SharedLayout from "./pages/SharedLayout";
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SharedLayout navbar={<Navbar />} />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="cocktails/:cocktailId" element={<SingleCocktail />} />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <h2>markdown preview starter</h2>
 }
 
-export default App;
+export default App
