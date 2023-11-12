792b1827f74e5f000a3940099ba13ec09e371b32
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-12 18:09:29 +0430

    Fetch Data and Display Users functionalities are completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+
+  return (
+    <main className="section-title">
+      <h1>{loading ? "loading..." : "Pagination"}</h1>
+      <div className="underline"></div>
+      <section className="followers">
+        <div className="container">
+          {data.map((item) => {
+            const { id, login: name, avatar_url: image, html_url: url } = item;
+            return <Follower key={id} data={{ name, image, url }} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/Follower.js
+++ src/Follower.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Follower = () => {
-  return <h2>follower component</h2>
-}
+const Follower = (props) => {
+  const { name, image, url } = props.data;
+  console.log();
+  return (
+    <article className="card">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <a href={url} className="btn">
+        view profile
+      </a>
+    </article>
+  );
+};
 
-export default Follower
+export default Follower;
