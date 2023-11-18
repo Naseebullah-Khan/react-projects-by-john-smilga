f55d75440dcb699299a3ffc1afd51ee3c2393202
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-18 20:27:56 +0430

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" type="button">
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((post) => {
+          return <Article key={post.id} {...post} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
