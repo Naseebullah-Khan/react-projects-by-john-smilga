45e2afc8af897571865859a83bac471346cbd196
Naseeb Khan <nk0784494104@gmail.com>
2023-11-19 16:30:49 +0430

    Changing theme functionality is added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import data from "./data";
 import Article from "./Article";
 
 function App() {
+  const [theme, setTheme] = useState("light-mode");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const themeHandler = () => {
+    if (theme === "light-mode") {
+      setTheme("dark-mode");
+    } else {
+      setTheme("light-mode");
+    }
+  };
+
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn" type="button">
+          <button className="btn" type="button" onClick={themeHandler}>
             toggle
           </button>
         </div>
