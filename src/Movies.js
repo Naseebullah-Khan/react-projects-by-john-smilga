d9d6658b68ddb8a5693c83d3d69e9d4311d3f881
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-20 20:36:00 +0430

    Movies component is completed

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  return <h2>movies component</h2>
-}
+  const { loading, movies } = useGlobalContext();
 
-export default Movies
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        const { imdbID, Title, Year, Poster } = movie;
+        return (
+          <Link key={imdbID} to={`/movies/${imdbID}`} className="movie">
+            <article>
+              <img src={Poster === "N/A" ? url : Poster} alt={Title} />
+              <div className="movie-info">
+                <h4>{Title}</h4>
+                <p>{Year}</p>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Movies;
