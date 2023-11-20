58fa2cb8ffb4973492950d04812cbf944285c210
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-20 18:13:31 +0430

    Initial Fetch functionality is added

--- src/context.js
+++ src/context.js
@@ -5,7 +5,39 @@ console.log(API_ENDPOINT);
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("batman");
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const fetchMovies = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.Response === "False") {
+        const err = { show: true, msg: `${data.Error}` };
+        setError(err);
+      } else {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ loading, movies, error, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
