441c282a291dade9685b8d09561409765dbd0b93
Naseeb Khan <nk0784494104@gmail.com>
2023-11-21 16:01:15 +0430

    Search component is completed

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,20 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  return <h2>search component</h2>
-}
+  const { error, setQuery, query } = useGlobalContext();
 
-export default SearchForm
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="search-form">
+      <h2>search movies</h2>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+        className="form-input"
+      />
+      {error.show && <p className="error">{error.msg}</p>}
+    </form>
+  );
+};
+
+export default SearchForm;
