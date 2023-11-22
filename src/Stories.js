538223c487aadef1f61ffae74670ad2fc4520848
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-22 20:04:56 +0430

    Stories component is completed

--- src/Stories.js
+++ src/Stories.js
@@ -3,11 +3,37 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { loading } = useGlobalContext();
+  const { loading, stories } = useGlobalContext();
 
   if (loading) return <section className="loading"></section>;
 
-  return <h2>stories component</h2>;
+  return (
+    <section className="stories">
+      {stories.map((story) => {
+        const { story_id, title, num_comments, points, author, url } = story;
+        return (
+          <article key={story_id} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span>
+              {num_comments} comments
+            </p>
+            <a
+              className="read-link"
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              read more
+            </a>
+            <button type="button" className="remove-btn">
+              remove
+            </button>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
