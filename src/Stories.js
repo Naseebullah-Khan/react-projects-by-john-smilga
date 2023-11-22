a5f94c1dacadd48781faa184f011abae63f772dc
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-11-22 18:11:10 +0430

    Basic Structure

--- src/Stories.js
+++ src/Stories.js
@@ -3,6 +3,10 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
+  const { loading } = useGlobalContext();
+
+  if (loading) return <section className="loading"></section>;
+
   return <h2>stories component</h2>;
 };
 
--- src/reducer.js
+++ src/reducer.js
@@ -6,5 +6,12 @@ import {
   HANDLE_SEARCH,
 } from "./actions";
 
-const reducer = () => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, loading: true };
+    default:
+      throw new Error(`no matching ${action.type} action type`);
+  }
+};
 export default reducer;
