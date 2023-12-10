4d1c734f41ae82e9679138bd4e1a8a41e9899c56
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-12-10 19:52:49 +0430

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard></Dashboard>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="*" element={<Error></Error>} />
+      </Routes>
+    </Router>
   );
 }
 
