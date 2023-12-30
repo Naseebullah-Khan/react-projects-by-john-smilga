a7b6f3ec8c0da2ba9f805485fa96b0cf597edf23
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2023-12-30 23:20:57 +0430

    Starter Project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./tutorial/App";
-import BookList from "./BookList";
-
-// CSS
 import "./index.css";
+import App from "./App";
+
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(<App />);
-root.render(<BookList />);
+root.render(<App />);
