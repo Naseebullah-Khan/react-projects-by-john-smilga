6fa1fe46069e3b8a8b070cbd2fb0d4332fa8c4cf
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-02-23 22:26:13 +0430

    Add AuthWrapper component to the App.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import PrivateRoute from "./PrivateRoute";
 import Products from "./ProductsPage";
 import SingleProduct from "./SingleProductPage";
 import Error from "./ErrorPage";
+import AuthWrapper from "./AuthWrapper";
 
 export {
   Home,
@@ -16,4 +17,5 @@ export {
   Products,
   SingleProduct,
   Error,
+  AuthWrapper,
 };
